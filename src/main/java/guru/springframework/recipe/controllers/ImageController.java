ff8f913c4f8528f872f17963bfe7d7f69cab5d40
Tagne  Paul Arnold <tagnearnold2080@gmail.com>
2021-06-16 10:13:36 +0100

    Added Image upload functionality with the controller, services and image Upload form Also added test for image uplod and form display

---
+++ src/main/java/guru/springframework/recipe/controllers/ImageController.java
@@ -0,0 +1,37 @@
+package guru.springframework.recipe.controllers;
+
+import guru.springframework.recipe.services.ImageService;
+import guru.springframework.recipe.services.RecipeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class ImageController {
+
+    private final ImageService imageService;
+    private final RecipeService recipeService;
+
+    public ImageController(ImageService imageService, RecipeService recipeService) {
+        this.imageService = imageService;
+        this.recipeService = recipeService;
+    }
+
+    @GetMapping("recipe/{id}/image")
+    public String showUploadForm(@PathVariable String id, Model model) {
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
+
+        return "recipe/imageuploadform";
+    }
+
+    @PostMapping("recipe/{id}/image")
+    public String handleImagePost(@PathVariable String id, @RequestParam("imagefile")MultipartFile file) {
+        imageService.saveImageFile(Long.valueOf(id), file);
+
+        return "redirect:/recipe/show/"+ id;
+    }
+}
---
+++ src/main/java/guru/springframework/recipe/services/ImageService.java
@@ -0,0 +1,7 @@
+package guru.springframework.recipe.services;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface ImageService {
+    void  saveImageFile(Long recipeId, MultipartFile file);
+}
