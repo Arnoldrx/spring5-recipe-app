e043ed93ed16cf1811306f7583f75c028f03e95e
Tagne  Paul Arnold <tagnearnold2080@gmail.com>
2021-06-30 15:56:08 +0100

    dislaying validation errors with thymeleaf

--- src/test/java/guru/springframework/recipe/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/recipe/controllers/RecipeControllerTest.java
@@ -78,22 +78,22 @@ public class RecipeControllerTest {
                 .andExpect(model().attributeExists("recipe"));
     }
 
-//    @Test
-//    public void testPostNewRecipeForm() throws Exception {
-//        RecipeCommand command = new RecipeCommand();
-//        command.setId(2L);
-//
-//        when(recipeService.saveRecipeCommand(any())).thenReturn(command);
-//
-//        mockMvc.perform(post("/recipe")
-//                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-//                .param("id", "")
-//                .param("description", "some string")
-//                .param("directions", "some directions")
-//        )
-//                .andExpect(status().is3xxRedirection())
-//                .andExpect(view().name("redirect:/recipe/show/2"));
-//    }
+    @Test
+    public void testPostNewRecipeForm() throws Exception {
+        RecipeCommand command = new RecipeCommand();
+        command.setId(2L);
+
+        when(recipeService.saveRecipeCommand(any())).thenReturn(command);
+
+        mockMvc.perform(post("/recipe")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("id", "")
+                .param("description", "some string")
+                .param("directions", "some directions")
+        )
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/recipe/show/2"));
+    }
 
     @Test
     public void testPostNewRecipeFormValidationFail() throws Exception {
